4ea77eb57ed8f0acf5b7dcf3f938068cc07eadb4
Jon Merrill <24jonnyboy24@gmail.com>
2016-01-31 16:19:38 -0800

    #10 finished except for state sync

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var ROSLIB = require('roslib');
+var exec = require('child_process').exec;
 
 // initialize our express web app
 var app = express();
@@ -14,11 +15,39 @@ app.use(bodyParser.json());
 // hold the states of all connected robots
 states = {};
 
-// accept a POST request at our base url
-// when we get a request, we enter the contained function
-app.post('/', function root (req, res) {
-    // get the id from the robot
-    var id = req.body.id;
+function disconnect_robot(id) {
+    // kill ROS process
+    var kill = exec('rosservice call kill turtle' + id, function (err, stdout, stderr){});
+    states[id].process.kill('SIGINT')
+    // clean up states Object
+    delete states[id];
+    
+}
+
+function check_timeouts(){
+    for (id in states) {
+        // if a robot hasn't sent a message in 2 minutes, delete it
+        if (states[id].last_connect < Date.now() - (120 * 1000)){
+            disconnect_robot(id);
+        }
+    }
+}
+
+// connect a robot. Returns null if all went well. Returns something if there was an err
+function connect_robot(id) {
+    if (Object.keys(states).length === 0) {
+        var turtle = exec('rosrun turtlesim turtlesim_node', function (err, stdout, stderr){
+            if (err || stderr){
+                return err || stderr
+            } 
+        });
+    } else {
+        var turtle = exec('rosservice call spawn 5 5 0 turtle' + id, function (err, stdout, stderr){
+            if (err || stderr){
+                return err || stderr
+            }
+        });
+    }
 
     // tap into a ros topic that we can talk to 
     // (Currently using turtlesim stuff for testing)
@@ -31,19 +60,46 @@ app.post('/', function root (req, res) {
     // update the state for the robot
     states[id] = {
         state: {},
-        talker: talker
+        talker: talker,
+        last_connect: Date.now(),
+        process: turtle
     }
 
-    // send a 200 response and end the request
-    res.sendStatus(200).end();
+    return null;
+}
+
+// accept a robot connection request at our base url
+app.post('/', function root (req, res) {
+    // get the id from the robot
+    var id = req.body.id;
+    if (id in states) {
+        console.log('here');
+        var err = 'Duplicate ID';
+    }
+
+    // connect the robot
+    var err2 = connect_robot(id);
+
+    if (err || err2) {
+        res.sendStatus(400).end(err || err2);
+    } else {
+        res.sendStatus(200).end();
+    }
 });
 
-// accept a POST request at <url>/command
-// when we get a request, we enter the contained function
+// accept a robot command at <url>/command
 app.post('/command', function command (req, res) {
     // get id and commands from robot
     var id = req.body.id;
-    var data = req.body.rosData;
+    var data = req.body.commands;
+
+    // In case server goes down, we can reconnect a robot without robot knowing
+    if (!(id in states)) {
+        var err = connect_robot(id);
+    }
+
+    states[id].last_connect = Date.now()
+    // upload state
 
     // construct a message of the correct type for our "talker" topic using POST data
     var twist = new ROSLIB.Message({
@@ -81,12 +137,22 @@ app.post('/command', function command (req, res) {
             states[id].state = message
             // send the state back to the robot and end the request
             res.end(JSON.stringify(message));
+
+            // now is a good time to check for timeouts
+            check_timeouts();
         }
     });
 
     // publish our message
     states[id].talker.publish(twist);
     published = 1;
+});
+
+// disconnect a robot when it asks to be disconnected
+app.post('/disconnect', function disconnect (req, res) {
+    var id = req.body.id;
+    disconnect_robot(id);
+    res.sendStatus(200).end();
 });
 
 // connect to ROS
